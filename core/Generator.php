6ca29b3e2fd80d148ccfb12ad42d8f86154cb178
Samodra <samodraland@yahoo.com>
2026-08-10 12:30:16 +0700

    Generate CSV format
    generate CSV format with optional header

--- core/Generator.php
+++ core/Generator.php
@@ -5,7 +5,7 @@ namespace Core;
 use Core\Utils;
 
 class Generator{
-
+    private const SEARCH = array("\r\n", "\n", "\r");
     public static function generateJSON( array $result, string $jsonpCallback = null ): string{
         $isJSONP = !is_null($jsonpCallback) ? true : false;
         $contentType = ( !$isJSONP ) ? "application/json" : "application/javascript";
@@ -25,9 +25,35 @@ class Generator{
         return $output;
     }
 
+    public static function generateCSV( array $result, bool $hasHeader = false ): string{
+        header("Content-Type: text/csv; charset=UTF-8");
+        http_response_code($result["status"]);
+
+        $records = $result["result"] ?? [];
+        $stream = fopen("php://temp", "r+");
+        // UTF-8 BOM improves compatibility with spreadsheet applications (e.g. Excel)
+        fwrite($stream, "\xEF\xBB\xBF");
+        $headerWritten = false;
+
+        foreach ($records as $row) {
+            if (is_array($row)) {
+                if ($hasHeader && !$headerWritten) {
+                    fputcsv($stream, array_keys($row));
+                    $headerWritten = true;
+                }
+                fputcsv($stream, array_values(str_replace(self::SEARCH, "", $row)));
+            }
+        }
+
+        rewind($stream);
+        $csv = stream_get_contents($stream);
+        fclose($stream);
+        return $csv;
+    }
+
     public static function generateXML( array $result ): string{
         header("Content-type: application/xml;charset=UTF-8");
-        http_response_code($finalResult["status"]);
+        http_response_code($result["status"]);
         $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><root/>');
         self::parseXML( $result, $xml);
         return $xml->asXML();			
@@ -45,11 +71,13 @@ class Generator{
                     self::parseXML($value, $subnode);
                 }
             } else {
+                // strip \r\n, \n, \r so they don't become &#13; / line breaks in the XML output
+                $cleanValue = str_replace(self::SEARCH, "", (string)$value);
                 if (is_numeric($key)) {
-                    $subnode = $xmlData->addChild("record", Utils::validateXmlString((string)$value));
+                    $subnode = $xmlData->addChild("record", Utils::validateXmlString($cleanValue));
                     $subnode->addAttribute("index", (string)$key);
                 } else {
-                    $xmlData->addChild($key, Utils::validateXmlString((string)$value));
+                    $xmlData->addChild($key, Utils::validateXmlString($cleanValue));
                 }
             }
         }
